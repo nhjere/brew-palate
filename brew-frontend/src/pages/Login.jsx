841a1922b9eead193d6f0bf35bc80d01ee8bd944
nhjere <nhjere@gmail.com>
2025-08-06 15:17:58 -0500

    persist session

--- brew-frontend/src/pages/Login.jsx
+++ brew-frontend/src/pages/Login.jsx
@@ -7,9 +7,14 @@ import { Link } from 'react-router-dom';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+    },
+})
 
 
 export default function Login() {
